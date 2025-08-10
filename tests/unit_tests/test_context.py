0f7de353eaeaf7015433131874a3639ccc5c62f9
Sydney Runkle <sydneymarierunkle@gmail.com>
2025-08-10 17:53:38 -0400

    unit tests

---
+++ tests/unit_tests/test_context.py
@@ -0,0 +1,17 @@
+from memory_agent.context import Context
+import os
+
+
+def test_context_init() -> None:
+    context = Context(user_id="test-user")
+    assert context.user_id == "test-user"
+
+def test_context_init_with_env_vars() -> None:
+    os.environ["USER_ID"] = "test-user"
+    context = Context()
+    assert context.user_id == "test-user"
+
+def test_context_init_with_env_vars_and_passed_values() -> None:
+    os.environ["USER_ID"] = "test-user"
+    context = Context(user_id="shouldn't override")
+    assert context.user_id == "test-user"
